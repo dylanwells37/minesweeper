2217dc5654817f95bf3b1c6700258c6f30bb6029
Dylan Wells <dylanwells37@gmail.com>
2023-02-09 16:14:19 -0500

    Add the adjacency values to the board

--- minesweeper.cpp
+++ minesweeper.cpp
@@ -5,22 +5,27 @@ using namespace std;
 
 int width = 10;
 int height = 10;
-int bomb_percentage = 10;
+int bomb_rate = 10; // 1 in bomb_rate cells will be a bomb
 vector<vector<int> > board (width, vector<int> (height, 0));
 void printBoard();
+bool isValid();
+void fill();
 
 int main(int argc, char* argv[]){
+    //Creates the initial board with bombs
     srand((unsigned) time(NULL));
     int random;
     for(int i = 0; i < width; i++){
         for(int j = 0; j < height; j++){
-            random = rand()%10;
+            random = rand()%5;
             if(random == 1){
-                board[i][j] = 1;
+                board[i][j] = -10;
             }
         }
     }
-    
+    //now to fill in the adjacencies
+    fill();
+    printBoard();
     
 
 }
@@ -33,3 +38,37 @@ void printBoard(){
         cout << endl;
     }
 }
+
+bool isValid(int i, int j){
+    if(i < 0 || j < 0 || j >=width || i >=height){
+        return false;
+    }
+    return true;
+}
+
+void fill(){
+    for(int i = 0;i < height; i++){
+        for(int j =0; j < width; j++){
+            if(board[i][j] < 0){
+                if(isValid(i-1, j-1)){
+                    board[i-1][j-1]++;
+                }if(isValid(i-1, j)){
+                    board[i-1][j]++;
+                }if(isValid(i-1, j+1)){
+                    board[i-1][j+1]++;
+                }if(isValid(i, j-1)){
+                    board[i][j-1]++;
+                }if(isValid(i, j+1)){
+                    board[i][j+1]++;
+                }if(isValid(i+1, j-1)){
+                    board[i+1][j-1]++;
+                }if(isValid(i+1, j)){
+                    board[i+1][j]++;
+                }if(isValid(i+1, j+1)){
+                    board[i+1][j+1]++;
+                }
+            }
+        }
+    }
+}
+
